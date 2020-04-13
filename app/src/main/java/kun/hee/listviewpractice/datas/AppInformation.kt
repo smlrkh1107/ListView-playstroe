ddae2d7be302cfc915d81659e5e2bcad44a1cd16
smlrkh1107 <krae7789230@gmail.com>
2020-04-13 21:14:09 +0900

    data 설정

--- app/src/main/java/kun/hee/listviewpractice/datas/AppInformation.kt
+++ app/src/main/java/kun/hee/listviewpractice/datas/AppInformation.kt
@@ -1,6 +1,6 @@
 package kun.hee.listviewpractice.datas
 
-class AppInformation(name:String, company:String, StarGrade:Int){
+class AppInformation(name:String, company:String, starGrade:Int){
     var name = name
     var company = company
     var starGrade = starGrade
