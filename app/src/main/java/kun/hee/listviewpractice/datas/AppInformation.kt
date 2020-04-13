d10000b7f2b55a5ef813184d85ed25abeb646f2c
smlrkh1107 <krae7789230@gmail.com>
2020-04-13 21:13:51 +0900

    Initial commit

---
+++ app/src/main/java/kun/hee/listviewpractice/datas/AppInformation.kt
@@ -0,0 +1,8 @@
+package kun.hee.listviewpractice.datas
+
+class AppInformation(name:String, company:String, StarGrade:Int){
+    var name = name
+    var company = company
+    var starGrade = starGrade
+
+}
