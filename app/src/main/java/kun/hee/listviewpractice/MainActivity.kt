a0d6450af159eccdad19882418ed7ff80c373033
smlrkh1107 <krae7789230@gmail.com>
2020-04-13 22:01:38 +0900

    여기까지하면 앱정보 화면이 리스트에 쫙뿌려지네.

--- app/src/main/java/kun/hee/listviewpractice/MainActivity.kt
+++ app/src/main/java/kun/hee/listviewpractice/MainActivity.kt
@@ -2,11 +2,35 @@ package kun.hee.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
+import kun.hee.listviewpractice.adapter.AppInfo_Adpater
+import kun.hee.listviewpractice.datas.AppInformation
 
 class MainActivity : AppCompatActivity() {
+
+    var mAppAdaper:AppInfo_Adpater? = null
+    val appList = ArrayList<AppInformation>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appList.add(AppInformation("아스팔트8:에어본","GameLoft",5))
+        appList.add(AppInformation("MineCraft ","Mojang ",4))
+        appList.add(AppInformation("아스팔트7:히트","GameLoft",4))
+        appList.add(AppInformation("팔라독 (Paladog)","FazeCat ",2))
+        appList.add(AppInformation("Plants vs. Zombies","EA Games",3))
+        appList.add(AppInformation("스왐피 (Swampy)","Disney ",5))
+        appList.add(AppInformation("리니지 2M","NCSOFT",3))
+        appList.add(AppInformation("리니지M ","NCSOFT ",4))
+        appList.add(AppInformation("AFK아레나","Lilith Games",1))
+
+        mAppAdaper = AppInfo_Adpater(this, R.layout.play_store_list, appList)
+
+        playStoreList.adapter = mAppAdaper
+
+
     }
 }
--- app/src/main/java/kun/hee/listviewpractice/adapter/AppInfo_Adpater.kt
+++ app/src/main/java/kun/hee/listviewpractice/adapter/AppInfo_Adpater.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kun.hee.listviewpractice.R
 import kun.hee.listviewpractice.datas.AppInformation
 
@@ -16,10 +17,25 @@ class AppInfo_Adpater(context:Context, resId:Int, list:ArrayList<AppInformation>
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow =convertView
+//        tempRow?.Let{ NULL이 아닐떄,}.Let{NULL 일때}
         if(tempRow == null) {
-            tempRow =inf.inflate(R.layout.play_store_list, null)
+            tempRow = inf.inflate(R.layout.play_store_list, null)
         }
+//        tempRow?.Let{
+//        }.Let{ tempRow = inf.inflate(R.layout.play_store_list, null)}
+//
 
+        val row = tempRow!!
+//      ★ 요기 위에 하나도 모르겠음 ★
+
+        val gameNameTxt = row.findViewById<TextView>(R.id.gameNameTxt)
+        val companyNameTxt = row.findViewById<TextView>(R.id.companyNameTxt)
+//        val s
+
+
+
+
+        return row
 
     }
 
